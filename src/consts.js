4416f4e747a982876e66e91c75b55a07c26f91bc
Sunil Chaudhary <sunil12738@gmail.com>
2018-07-07 13:26:42 +0530

    Fix error

--- src/consts.js
+++ src/consts.js
@@ -1,6 +1,6 @@
 module.exports = {
-  // s3BucketName: process.env.S3_BUCKET_NAME,
-  s3BucketPath: process.env.S3_BUCKET_PATH,
+  s3BucketName: process.env.S3_BUCKET_NAME,
+  // s3BucketPath: process.env.S3_BUCKET_PATH,
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   folderNameToUpload: process.env.FOLDER_NAME,
